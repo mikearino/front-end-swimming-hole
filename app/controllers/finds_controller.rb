e206ff2c7db901092d93735a7173545f10e3ea00
Student <example@example.com>
2019-08-21 14:05:02 -0700

    successfully call API

--- app/controllers/finds_controller.rb
+++ app/controllers/finds_controller.rb
@@ -1,8 +1,9 @@
-class SwimFinderController <ApplicationController
+
+class FindsController < ApplicationController
   def index
     response = RestClient::Request.execute(
       method: :get,
-      url: 'localhost:3000/v1/locations',
+      url: 'localhost:3001/v1/locations',
       headers: {}
     )
     @locations = JSON.parse(response)
