02a6b5c22708843c7d9ad969dcbfbad4107f9148
Student <example@example.com>
2019-08-21 14:51:35 -0700

    add create method with hard coded info to successfully test post method to API

--- app/controllers/finds_controller.rb
+++ app/controllers/finds_controller.rb
@@ -9,4 +9,18 @@ class FindsController < ApplicationController
     @locations = JSON.parse(response)
   end
 
+  def create
+    RestClient.post "localhost:3001/v1/locations",
+    {name: 'Burger King', place: 'Oregon', content: 'Organic farm to table chicken fries with balsamic vinnegrete'}
+    redirect_to finds_path
+  end
+
+  # def delete
+  #   RestClient.delete(
+  #     url: "localhost:3001/v1/locations/21".
+  #     headers: {}
+  #   )
+  #   redirect_to finds_path
+  # end
+
 end
